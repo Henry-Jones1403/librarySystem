aa6b1dfe7036aae46d39b53a66d2128c08a20cfd
hj210472 <hj210472@reigate.ac.uk>
2022-01-21 13:28:50 +0000

    more objects

--- src/com/company/functions/WelcomeTransfer.java
+++ src/com/company/functions/WelcomeTransfer.java
@@ -10,10 +10,30 @@ public class WelcomeTransfer {
         ArrayList<bookObjects> booksList = new ArrayList<>();
         try {
             Scanner reader = new Scanner(bookFile);
-        }catch(Exception e){
+            while(reader.hasNextLine()) {
+                String line = reader.nextLine();
+                String[] splitter = line.split(",");
+                bookObjects addBook = new bookObjects(splitter[0], Integer.parseInt(splitter[1]), splitter[2], splitter[3]);
+                booksList.add(addBook);
+            }
+        } catch(Exception e){
             System.out.println("Please restart programme, there has been an issue!");
             System.exit(0);
-bookObject newBook = new bookObjects()
+        }
+    }    private static void borrowerTransfer(File bookFile){
+        ArrayList<bookObjects> booksList = new ArrayList<>();
+        try {
+            Scanner reader = new Scanner(bookFile);
+            while(reader.hasNextLine()) {
+                String line = reader.nextLine();
+                String[] splitter = line.split(",");
+                bookObjects addBook = new bookObjects(splitter[0], Integer.parseInt(splitter[1]), splitter[2], splitter[3]);
+                booksList.add(addBook);
+            }
+        } catch(Exception e){
+            System.out.println("Please restart programme, there has been an issue!");
+            System.exit(0);
+            bookObjects newBook = new bookObjects("RTG", 324234, "", "");
         }
     }}
 //use the same code from the deleter and put it into a object then into the booklist and the do it all again for the borrowers
